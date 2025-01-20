b7114c684676cf29a4ea58b4b66f7329abe624e7
QManiac162 <watcher1895@gmail.com>
2025-01-20 22:16:55 +0530

    added in IEEE project

--- GeoQuery-frontend/src/assets/index.js
+++
@@ -1,5 +0,0 @@
-import ellipse from "./images/ellipse.svg";
-import hero from "./images/hero1.svg";
-import merge from "./images/merge.png";
-import map from "./images/map.png";
-export { ellipse, hero, merge,map };
